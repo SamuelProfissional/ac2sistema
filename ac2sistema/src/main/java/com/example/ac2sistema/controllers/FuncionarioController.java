71e7cd0095d2932ff2d32964153f538ec69882e6
Caio Souza <caio.souza@huggy.io>
2025-10-29 18:04:53 -0300

    Alterações no Frontand e removendo testes

--- ac2sistema/src/main/java/com/example/ac2sistema/controllers/FuncionarioController.java
+++ ac2sistema/src/main/java/com/example/ac2sistema/controllers/FuncionarioController.java
@@ -2,13 +2,11 @@ package com.example.ac2sistema.controllers;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.ac2sistema.dtos.DadosProjetoDTO;
@@ -26,7 +24,6 @@ public class FuncionarioController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public void adicionar(@RequestBody FuncionarioDTO funcionarioDTO) {
         funcionarioService.salvar(funcionarioDTO);
     }
--- ac2sistema/src/main/java/com/example/ac2sistema/controllers/SetorController.java
+++ ac2sistema/src/main/java/com/example/ac2sistema/controllers/SetorController.java
@@ -2,15 +2,12 @@ package com.example.ac2sistema.controllers;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
-
 import com.example.ac2sistema.dtos.DadosSetorDTO;
 import com.example.ac2sistema.dtos.SetorDTO;
 import com.example.ac2sistema.services.SetorService;
@@ -26,7 +23,6 @@ public class SetorController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
     public void adicionar(@RequestBody SetorDTO setorDTO) {
         setorService.salvar(setorDTO);
     }
